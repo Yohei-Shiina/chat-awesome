374a7990d2ab6b6eb0d9f77457420ac4bfb19d58
Yohei Shiina <ysys.j.0125@gmail.com>
2022-10-01 17:11:56 +0000

    typescriptize [...nextauth]

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
@@ -10,16 +10,14 @@ export const authOptions = {
     // ...add more providers here
   ],
   callbacks: {
-    async jwt(params) {
-      const { token, account } = params;
+    async jwt({ token, account }) {
       // Persist the OAuth access_token to the token right after signin
       if (account) {
         token.accessToken = account.access_token;
       }
       return token;
     },
-    async session(params) {
-      const { session, token, user } = params;
+    async session({ session, token, user }) {
       // Send properties to the client, like an access_token from a provider.
       session.accessToken = token.accessToken;
       return session;
