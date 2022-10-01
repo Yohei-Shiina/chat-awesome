3e49bb60307b1f71e69c38496a151007256f93db
Yohei Shiina <ysys.j.0125@gmail.com>
2022-10-01 17:12:19 +0000

    enable useSession in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.scss'
+import React from 'react'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/globals.scss'
+import { Session } from 'next-auth'
+
+function App({
+   Component, 
+   pageProps: { session, ...pageProps } 
+}: (AppProps<{session: Session}>)) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
-export default MyApp
+export default App
