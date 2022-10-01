49c99af5e45f365ac1c6c9cbb9cff699c50f821c
Yohei Shiina <ysys.j.0125@gmail.com>
2022-10-01 17:57:22 +0000

    refactor _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
+import type { Session } from 'next-auth'
 import { SessionProvider } from "next-auth/react"
 
 import '../styles/globals.scss'
-import { Session } from 'next-auth'
 
-function App({
-   Component, 
-   pageProps: { session, ...pageProps } 
-}: (AppProps<{session: Session}>)) {
+type Props = AppProps<{session: Session}>
+
+function App({ Component, pageProps }: Props) {
+  const { session } = pageProps;
+
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
