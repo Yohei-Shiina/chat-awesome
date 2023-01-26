e5bd0e4690ec5252bc1333dab9873a6c75008c11
Yohei Shiina <ysys.j.0125@gmail.com>
2023-01-26 09:50:47 +0000

    add chakraUI package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import type { AppProps } from 'next/app'
-import type { Session } from 'next-auth'
-import { SessionProvider } from "next-auth/react"
+import React from "react";
+import type { AppProps } from "next/app";
+import type { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import "~/styles/globals.scss";
 
-import '~/styles/globals.scss'
-
-type Props = AppProps<{session: Session}>
+type Props = AppProps<{ session: Session }>;
 
 function App({ Component, pageProps }: Props) {
   const { session } = pageProps;
 
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  )
+    <ChakraProvider>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ChakraProvider>
+  );
 }
 
-export default App
+export default App;
